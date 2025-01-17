244664f35b57ba12497fccc6b65fbe3c8733da87
cho6019 <j01040864205@gmail.com>
2025-01-17 10:54:34 +0900

    version up and add image

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -10,14 +10,14 @@ export default defineUserConfig({
   lang: 'en-US',
 
   title: 'team SUNSIN',
-  description: "cashmallow dev team's technology blog about wanting to contribute to the open source ecosystem",
+  description: "cho's blog",
 
   head: [
     ['meta', { property: 'og:type', content: 'website' }],
-    ['meta', { property: 'og:title', content: 'tech-blog:cashmallow' }],
-    ['meta', { property: 'og:description', content: "cashmallow dev team's technology blog about wanting to contribute to the open source ecosystem" }],
-    ['meta', { property: 'og:image', content: 'https://user-images.githubusercontent.com/120996497/209457891-2c91f7fe-514d-4431-bfcb-7f6ee72ed8ee.png' }],
-    ['meta', { property: 'og:url', content: 'https://oss.cashmallow.com' }],
+    ['meta', { property: 'og:title', content: 'chos blog' }],
+    ['meta', { property: 'og:description', content: "I'm cho min kyu. just put your thumbs up. T H U M B S" }],
+    ['meta', { property: 'og:image', content: 'https://ppabam.github.io/images/repository-open-graph-template.png' }],
+    ['meta', { property: 'og:url', content: 'https://cho6019.cho6019.store' }],
   ],
   
   locales: {
